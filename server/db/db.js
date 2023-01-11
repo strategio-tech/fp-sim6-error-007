e03a16d868325c722dfbe310b3dd55db55884a54
Kendall Perry <perry.kendall@gmail.com>
2023-01-11 13:10:17 -0500

    db file cleaned up, variables stored in env for sequelize connection to postgres in aws

--- server/db/db.js
+++ server/db/db.js
@@ -1,34 +1,10 @@
 const Sequelize = require('sequelize')
-const pkg = require('../../package.json')
 
-const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
-
-const config = {
-  logging: false
-};
-
-if(process.env.LOGGING === 'true'){
-  delete config.logging
-}
-
-//https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
-if(process.env.DATABASE_URL){
-  config.dialectOptions = {
-    ssl: {
-      rejectUnauthorized: false
-    }
-  };
-}
-
-const user = 'postgres'
-const host = 'creature-coders-1.c4ywbhfyodwd.us-east-1.rds.amazonaws.com'
-const database = 'postgres'
-const password = 'teamerror007'
-const port = '5432'
-
-// This is for connecting to local host, 
-// const db = new Sequelize(
-//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config)
+const user = process.env.user
+const host = process.env.host
+const database = process.env.database
+const password = process.env.password
+const port = process.env.port
 
 const db = new Sequelize(database, user, password, {
   host,
