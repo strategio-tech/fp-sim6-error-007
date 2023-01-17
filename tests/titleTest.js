aba6872d3bd0ef19306cb3e834103a8b3647edc2
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2023-01-17 15:35:39 -0800

    github actions test 4

--- tests/titleTest.js
+++ tests/titleTest.js
@@ -1,10 +1,15 @@
 const {Builder} = require ("selenium-webdriver");
 const chrome = require('selenium-webdriver/chrome');
 
+const screen = {
+    width: 640,
+    height: 480
+  };
+
 async function titleTest() {
     let driver = await new Builder()
     .forBrowser("chrome")
-    .setChromeOptions(new chrome.Options().headless())
+    .setChromeOptions(new chrome.Options().headless().windowSize(screen))
     .build();
     try {
         await driver.get('http://localhost:8080');
